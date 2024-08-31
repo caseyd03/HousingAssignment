06269f2fa45b51bd7b648cbf35fd182d3a615d01
Casey Hartley <122581730+caseyd03@users.noreply.github.com>
2024-08-31 13:51:47 -0700

    Create hud.py

---
+++ hud.py
@@ -0,0 +1,14 @@
+#This is homeless data for San Luis Obispo County, categorized into age brackets. 
+
+age_data = {
+  "Under 18": 246,
+  "Age 18 to 24": 90.
+  "Age 25 to 34": 62,
+  "Age 35 to 44": 51,
+  "Age 45 to 54": 49,
+  "Age 55 to 64": 53,
+  "Over 60": 30
+}
+
+  
+  
